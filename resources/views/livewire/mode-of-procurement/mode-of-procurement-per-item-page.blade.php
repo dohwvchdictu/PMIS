6dc73d48ce959759277f65ed0796ddf7638dcd33
mjlscna <mjlascuna.02@gmail.com>
2026-02-26 14:30:15 +0800

    fixed forward to PMU for existing PR with Stage Status Forwarded to PMU

--- resources/views/livewire/mode-of-procurement/mode-of-procurement-per-item-page.blade.php
+++ resources/views/livewire/mode-of-procurement/mode-of-procurement-per-item-page.blade.php
@@ -124,8 +124,7 @@
                                         <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                 d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                                        </svg>
-                                        Bulk Edit
+                                        </svg>Edit
                                     </span>
                                 </button>
                             </div>
@@ -1181,8 +1180,7 @@
                                             viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                 d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                                        </svg>
-                                        Bulk Edit
+                                        </svg>Edit
                                     </span>
                                 </button>
                                 @if ($this->canForwardToPmu)
@@ -1303,6 +1301,7 @@
                                             $forwardedStage = $prItemID
                                                 ? \App\Models\PrItemPrstage::where('prItemID', $prItemID)
                                                     ->where('pr_stage_id', 7)
+                                                    ->whereNotNull('actual_date_forwarded')
                                                     ->orderBy('id', 'desc')
                                                     ->first()
                                                 : null;
