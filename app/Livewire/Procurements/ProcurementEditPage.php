2828b9da6290ddcef936f1f79d3b2e9f6e4aba3b
mjlscna <mjlascuna.02@gmail.com>
2026-03-11 10:23:49 +0800

    fixed order of drop down select in procurement module

--- app/Livewire/Procurements/ProcurementEditPage.php
+++ app/Livewire/Procurements/ProcurementEditPage.php
@@ -470,13 +470,13 @@ class ProcurementEditPage extends Component
     public function render()
     {
         return view('livewire.procurements.procurement-edit-page', [
-            'divisions' => Division::all(),
-            'categories' => Category::with(['categoryType', 'bacType'])->get(),
+            'divisions' => Division::orderBy('divisions')->get(),
+            'categories' => Category::with(['categoryType', 'bacType'])->orderBy('category')->get(),
             'clusterCommittees' => ClusterCommittee::all(),
-            'venueSpecifics' => VenueSpecific::all(),     // <-- restored original names
-            'venueProvinces' => ProvinceHuc::all(),
+            'venueSpecifics' => VenueSpecific::orderBy('name')->get(),
+            'venueProvinces' => ProvinceHuc::orderBy('province_huc')->get(),
             'endUsers' => EndUser::all(),
-            'fundSources' => FundSource::all(),
+            'fundSources' => FundSource::orderBy('fundsources')->get(),
             'form' => $this->form,
         ]);
     }
